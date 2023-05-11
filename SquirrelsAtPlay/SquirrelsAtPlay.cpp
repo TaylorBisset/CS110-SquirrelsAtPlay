c5f1562bc35c8aa4865bda23075ab68a8f03345d
TaylorBisset <87632154+TaylorBisset@users.noreply.github.com>
2023-05-11 13:50:40 -0700

    Created initial variables for summer and temperature. Created user input prompts.

--- SquirrelsAtPlay/SquirrelsAtPlay.cpp
+++ SquirrelsAtPlay/SquirrelsAtPlay.cpp
@@ -4,5 +4,21 @@ using namespace std;
 
 int main()
 {
+    char isSummer = 'n';
+    int temperature = 70;
     
+    cout << "Is it Summer?\n(y/n) : ";
+    cin >> isSummer;
+
+    cout << "What is the temperature in degrees Fahrenheit? ";
+    cin >> temperature;
+    if (temperature > 60)
+    {
+        if ((isSummer == 'n' && temperature < 90) || (isSummer == 'y' && temperature < 100))
+        {
+            cout << endl << "The squirrels are out playing!\n";
+        }
+        
+    }
+        
 }
